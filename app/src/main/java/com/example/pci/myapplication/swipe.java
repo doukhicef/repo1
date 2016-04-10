9416655a30c2efd5d66d84d5e3f004547684637e
Youcef DOUKHI <ydoukhi@srvmail.sarpitest.tk>
2016-04-10 16:13:50 +0100

    Commit 3

---
+++ app/src/main/java/com/example/pci/myapplication/swipe.java
@@ -0,0 +1,31 @@
+package com.example.pci.myapplication;
+
+import java.util.List;
+import java.util.Vector;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+
+public class swipe extends FragmentActivity {
+
+    private PagerAdapter mPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.swipe);
+
+        List fragments = new Vector();
+
+        fragments.add(Fragment.instantiate(this, frag1.class.getName()));
+
+
+        this.mPagerAdapter = new MyPagerAdapter(super.getSupportFragmentManager(), fragments);
+
+        ViewPager pager = (ViewPager) super.findViewById(R.id.viewpager);
+        pager.setAdapter(this.mPagerAdapter);
+    }
+}
